bc9bd0d5680c025a5ff7a6d4402eeb612566df55
NofireHavechills <20234196@med.chosun.ac.kr>
2026-02-05 01:42:41 +0900

    Change base path in Vite config to rootㄹㄹㄹ
    ㅇㅇㅇ

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  base: "/Yoonju-J.github.io/",
+  base: "/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
